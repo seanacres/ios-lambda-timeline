1df0c653ef6c2355a2f226461a74168c5a8154ac
seanacres <6416690+seanacres@users.noreply.github.com>
2020-07-11 12:18:33 -0400

    Implemented basic ability to record in an audio comment

--- LambdaTimeline/View Controllers/CommentViewController.swift
+++ LambdaTimeline/View Controllers/CommentViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CommentViewController: UIViewController {
 
@@ -16,21 +17,143 @@ class CommentViewController: UIViewController {
     
     var isAudioComment: Bool = true
     
+    weak var timer: Timer?
+    
+    var recordingURL: URL?
+    var audioRecorder: AVAudioRecorder?
+    
+    private lazy var timeIntervalFormatter: DateComponentsFormatter = {
+        let formatting = DateComponentsFormatter()
+        formatting.unitsStyle = .positional // 00:00  mm:ss
+        formatting.zeroFormattingBehavior = .pad
+        formatting.allowedUnits = [.minute, .second]
+        return formatting
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        updateViews()
-    }
-    
-    private func updateViews() {
+        
         if isAudioComment {
             commentTextField.isHidden = true
         } else {
             recordingTimeLabel.isHidden = true
             recordButton.isHidden = true
+            recordingTimeLabel.font = UIFont.monospacedDigitSystemFont(ofSize: recordingTimeLabel.font.pointSize,
+            weight: .regular)
         }
     }
 
+    private func updateViews() {
+        recordButton.isSelected = isRecording
+        let elapsedTime = audioRecorder?.currentTime ?? 0
+        recordingTimeLabel.text = timeIntervalFormatter.string(from: elapsedTime)!
+    }
+    
+    // MARK: - Timer
+    
+    deinit {
+        timer?.invalidate()
+    }
+    
+    func startTimer() {
+        timer?.invalidate()
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.030, repeats: true) { [weak self] (_) in
+            guard let self = self else { return }
+            
+            self.updateViews()
+            
+        }
+    }
+    
+    func cancelTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    
+    // MARK: - Recording
+        
+    var isRecording: Bool {
+        audioRecorder?.isRecording ?? false
+    }
+    
+    func prepareAudioSession() throws {
+        let session = AVAudioSession.sharedInstance()
+        try session.setCategory(.playAndRecord, options: [.defaultToSpeaker])
+        try session.setActive(true, options: []) // can fail if on a phone call, for instance
+    }
+        
+    func createNewRecordingURL() -> URL {
+        let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        
+        let name = ISO8601DateFormatter.string(from: Date(), timeZone: .current, formatOptions: .withInternetDateTime)
+        let file = documents.appendingPathComponent(name, isDirectory: false).appendingPathExtension("caf")
+        
+//        print("recording URL: \(file)")
+        
+        return file
+    }
+    
+    func requestPermissionOrStartRecording() {
+        switch AVAudioSession.sharedInstance().recordPermission {
+        case .undetermined:
+            AVAudioSession.sharedInstance().requestRecordPermission { granted in
+                guard granted == true else {
+                    print("We need microphone access")
+                    return
+                }
+                
+                print("Recording permission has been granted!")
+                // NOTE: Invite the user to tap record again, since we just interrupted them, and they may not have been ready to record
+            }
+        case .denied:
+            print("Microphone access has been blocked.")
+            
+            let alertController = UIAlertController(title: "Microphone Access Denied", message: "Please allow this app to access your Microphone.", preferredStyle: .alert)
+            
+            alertController.addAction(UIAlertAction(title: "Open Settings", style: .default) { (_) in
+                UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+            })
+            
+            alertController.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+            
+            present(alertController, animated: true, completion: nil)
+        case .granted:
+            startRecording()
+        @unknown default:
+            break
+        }
+    }
+    
+    func startRecording() {
+        do {
+            try prepareAudioSession()
+        } catch {
+            print("Cannot record audio \(error)")
+            return
+        }
+        
+        recordingURL = createNewRecordingURL()
+        let format = AVAudioFormat(standardFormatWithSampleRate: 44_100, channels: 1)!
+        
+        do {
+            audioRecorder = try AVAudioRecorder(url: recordingURL!, format: format)
+            audioRecorder?.delegate = self
+            audioRecorder?.record()
+            updateViews()
+            startTimer()
+        } catch {
+            preconditionFailure("Audio recorder could not be created with \(recordingURL!) and format: \(format) and error: \(error)")
+        }
+    }
+    
+    func stopRecording() {
+        audioRecorder?.stop()
+        updateViews()
+        cancelTimer()
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -42,8 +165,27 @@ class CommentViewController: UIViewController {
     */
 
     @IBAction func recordButtonTapped(_ sender: Any) {
+        if isRecording {
+            stopRecording()
+        } else {
+            requestPermissionOrStartRecording()
+        }
     }
     
     @IBAction func saveButtonTapped(_ sender: Any) {
+        
+    }
+}
+
+extension CommentViewController: AVAudioRecorderDelegate {
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        audioRecorder = nil
+        cancelTimer()
+    }
+    
+    func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder, error: Error?) {
+        if let error = error {
+            print("error: \(error)")
+        }
     }
 }
